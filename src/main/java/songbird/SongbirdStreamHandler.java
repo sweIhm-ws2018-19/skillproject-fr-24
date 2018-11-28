7bfc76fb521c63322df1686416ffe1a5a4a8e142
Hempfle <cweber@hm.edu>
2018-11-28 17:55:22 +0100

    funktionierender Audiolink eingefügt

--- src/main/java/songbird/SongbirdStreamHandler.java
+++ src/main/java/songbird/SongbirdStreamHandler.java
@@ -26,6 +26,7 @@ public class SongbirdStreamHandler extends SkillStreamHandler {
         return Skills.standard()
                 .addRequestHandlers(
                         new StimmeIntentHandler(),
+                        new IntervallTrainingIntentHandler(),
                         new AudioTestHandler(),
                         new KoloraturenTrainingIntentHandler(),
                         new LaunchRequestHandler(),

6fad6b8c6e6518ec47aae6fad7a2fc56445e97f5
ShinyKarpador <43878914+ShinyKarpador@users.noreply.github.com>
2018-11-28 19:29:43 +0100

    Tipps intent

--- src/main/java/songbird/handlers/TippsIntentHandler.java
+++ src/main/java/songbird/handlers/TippsIntentHandler.java
@@ -3,6 +3,10 @@ package songbird.handlers;
 import com.amazon.ask.dispatcher.request.handler.HandlerInput;
 import com.amazon.ask.dispatcher.request.handler.RequestHandler;
 import com.amazon.ask.model.*;
+import com.amazon.ask.model.Response;
+import com.amazon.ask.response.ResponseBuilder;
+import songbird.lists.TipList;
+
 import java.util.Optional;
 
 import static com.amazon.ask.request.Predicates.intentName;
@@ -10,20 +14,19 @@ import static com.amazon.ask.request.Predicates.intentName;
 public class TippsIntentHandler implements RequestHandler{
     @Override
     public boolean canHandle(HandlerInput input) {
-        return input.matches(intentName("AMAZON.TippsIntent"));
+        return input.matches(intentName("TippsIntent"));
     }
 
     @Override
     public Optional<Response> handle(HandlerInput input) {
 
+        TipList tip = new TipList();
 
-        String tippsText = "test, Koloraturen";
-
+        String speechText = tip.getRandomTip();
+        speechText += "Möchtest du jetzt an deiner Stimme arbeiten oder mehr Tipps?";
 
         return input.getResponseBuilder()
-                .withSimpleCard("title", "cardText")
-                .withSpeech(tippsText)
-                .withReprompt("bar")
+                .withSpeech(speechText)
                 .withShouldEndSession(false).build();
     }
 }

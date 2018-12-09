838b2b7bd140f3d6cf51b7a359f59539e836a76d
Hempfle <cweber@hm.edu>
2018-12-09 19:16:22 +0100

    funktionierender Audiolink eingefügt

--- src/main/java/songbird/handlers/IntervallTrainingIntentHandler.java
+++ src/main/java/songbird/handlers/IntervallTrainingIntentHandler.java
@@ -25,13 +25,13 @@ public class IntervallTrainingIntentHandler implements RequestHandler {
         Intent intent = intentRequest.getIntent();
         String output = "Spiele Intervalltraining.";
         String path = "https://s3.amazonaws.com/songbirdswe/testaudio.mp3";
-
+        /*
         Stream audioStream = Stream.builder().withUrl(path).withToken("test.mp3").withOffsetInMilliseconds(Long.valueOf(0)).build();
         AudioItem item = AudioItem.builder().withStream(audioStream).build();
         AudioPlayerState state = AudioPlayerState.builder().withToken("test.mp3").withPlayerActivity(PlayerActivity.PLAYING).build();
         PlayDirective directive = PlayDirective.builder().withAudioItem(item).build();
-
-        return input.getResponseBuilder().withSpeech(output).withShouldEndSession(false).build();
+        */
+        return input.getResponseBuilder().withSpeech(output).withShouldEndSession(true).build();
         //return input.getResponseBuilder().withSpeech(output).addAudioPlayerPlayDirective(directive.getPlayBehavior(), Long.valueOf(0), null, "test.mp3", path).build();
     }
 }
